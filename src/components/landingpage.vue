43f171cd378f667a162d6c3cf3ce3dd88d69a0a8
TaoYu <121595113@qq.com>
2018-06-08 17:47:56 +0800

    修改按钮文案

--- src/components/landingpage.vue
+++ src/components/landingpage.vue
@@ -82,7 +82,8 @@ export default {
             }
             if (res.data.funding === 1) {
               AlertModule.show({
-                content: 'You have an outstanding loan. You may borrow again once this loan has been paid.'
+                content: 'You have an outstanding loan. You may borrow again once this loan has been paid.',
+                'buttonText': 'OK'
               })
             }
           })
