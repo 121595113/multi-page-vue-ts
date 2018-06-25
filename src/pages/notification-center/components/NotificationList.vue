b2931074f93a012049117ce336562ca51f267b2b
Khan Fan <khan.fan@oriente.com>
2018-06-25 14:52:02 +0800

    只有 pullup

--- src/pages/notification-center/components/NotificationList.vue
+++ src/pages/notification-center/components/NotificationList.vue
@@ -4,12 +4,13 @@
     :style="{ height: wrapperHeight + 'px'}"
     class="notification-list page-loadmore-wrapper">
     <mt-loadmore
-        :top-method="loadTop"
         :bottom-method="loadBottom"
         :bottom-all-loaded="allLoaded"
-        :auto-fill="false"
-        @top-status-change="handleTopChange"
         @bottom-status-change="handleBottomChange"
+        :auto-fill="false"
+        :bottomPullText="bottomPullText"
+        :bottomDropText="bottomDropText"
+        :bottomLoadingText="bottomLoadingText"
         ref="loadmore">
       <ul class="list" v-if="notificationList.length > 0">
         <li v-for="item in notificationList" :key="item.notificationId">
@@ -26,18 +27,6 @@
         </li>
       </ul>
       <Empty v-else emptyType="NoticeListEmpty" tipText='<p>You do not have any notification.</p>' />
-      <div slot="top" class="mint-loadmore-top">
-        <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
-        <span v-show="topStatus === 'loading'">
-          <mt-spinner type="snake"></mt-spinner>
-        </span>
-      </div>
-      <div slot="bottom" class="mint-loadmore-bottom">
-        <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
-        <span v-show="bottomStatus === 'loading'">
-          <mt-spinner type="snake"></mt-spinner>
-        </span>
-      </div>
     </mt-loadmore>
   </div>
 </template>
@@ -47,8 +36,6 @@ import { mapMutations } from 'vuex';
 import { Loadmore, Spinner } from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import Empty from '../../../oriente-ui/Empty';
-console.log(123);
-console.log(Empty);
 export default {
   data () {
     return {
@@ -56,44 +43,10 @@ export default {
       bottomStatus: '',
       wrapperHeight: 0,
       allLoaded: false,
-      notificationList: [
-        // {
-        //   'notificationId': 1,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // },
-        // {
-        //   'notificationId': 2,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up a</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // },
-        // {
-        //   'notificationId': 3,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up b</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // },
-        // {
-        //   'notificationId': 4,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // },
-        // {
-        //   'notificationId': 5,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up a</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // },
-        // {
-        //   'notificationId': 6,
-        //   'title': 'Tfpny Rnkr Scimxvlj Fvuixi Wlrwcma Bvyoplqr Ztbajsyc Srnxwosgr',
-        //   'template': '<p>Cashalo is <a href="http://m.baidu.com/">sign up b</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-        //   'effectiveAt': 'Jun 23,1998 23:59'
-        // }
-      ]
+      bottomPullText: '↑ Pull up',
+      bottomDropText: '↓ Release',
+      bottomLoadingText: 'Loading',
+      notificationList: [],
     }
   },
   components: {
@@ -120,43 +73,23 @@ export default {
   },
   mounted () {
     this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top;
-    // document.querySelector('.mint-loadmore-content').style.height = '100%';
     this.setTitle('Notifications');
     this.fetchData();
   },
   methods: {
-    handleTopChange (status) {
-      console.log(status);
-      this.topStatus = status;
-    },
     handleBottomChange (status) {
       console.log('handleBottomChange ', status);
       this.bottomStatus = status;
     },
-    loadTop () {
-      console.log('loadTop');
-      setTimeout(() => {
-        // let firstValue = this.notificationList[0];
-        for (let i = 1; i <= 2; i++) {
-          this.notificationList.unshift({
-            notificationId: i * 10,
-            title: 'This is Title',
-            template: '<p>Cashalo is <a href="http://m.baidu.com/">sign up b</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
-            effectiveAt: 'Jun 23,1998 23:59',
-          });
-        }
-        this.$refs.loadmore.onTopLoaded();
-        console.log('load top end');
-      }, 2000);
-    },
     loadBottom () {
+      const that = this;
       setTimeout(() => {
-        let lastValue = this.notificationList[this.notificationList.length - 1];
-        if (lastValue <= 60) {
-          for (let i = 1; i <= 10; i++) {
-            this.notificationList.push({
-              notificationId: i * 100,
-              title: 'This is Title',
+        let lastValue = that.notificationList.length;
+        if (lastValue <= 6) {
+          for (let i = 1; i <= 1; i++) {
+            that.notificationList.push({
+              notificationId: (+new Date() + i * 100),
+              title: 'This is Title This is Title',
               template: '<p>Cashalo is <a href="http://m.baidu.com/">sign up b</a> on beta testing so we currently only have pre-approved users and partners on the platform. If you are interested in using Cashalo once we launch to the public, <a href="#">sign up</a>to get the latest updates.</p>',
               effectiveAt: 'Jun 23,1998 23:59',
             });
@@ -265,30 +198,10 @@ export default {
   line-height: 50px;
   border-bottom: 1px solid #eee;
 }
-// .mint-loadmore {
-//   height: 100%;
-// }
-// .mint-loadmore-content {
-//   height: 100%;
-// }
-.mint-loadmore-top {
-  span {
-    display: inline-block;
-    transition: .2s linear;
-    vertical-align: middle;
-  }
-  span.is-rotate {
-    transform: rotate(180deg);
-  }
-}
 .mint-loadmore-bottom {
-  span {
-    display: inline-block;
-    transition: .2s linear;
-    vertical-align: middle;
-    }
-    span.is-rotate {
-      transform: rotate(180deg);
-    }
+  .mint-loadmore-text {
+    font-size: 12px;
+    color: rgba(0,0,0,0.50);
+  }
 }
 </style>
