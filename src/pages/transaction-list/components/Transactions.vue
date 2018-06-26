6ed3793e10a21d3342e98f6a14693a1d10095b1d
Khan Fan <khan.fan@oriente.com>
2018-06-26 11:23:16 +0800

    add UI

--- src/pages/transaction-list/components/Transactions.vue
+++ src/pages/transaction-list/components/Transactions.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="transactios">hello <Tag /></div>
+  <div class="transactios">
+    <div class="test">
+      hello
+      <Tag type="disburment" />
+      world
+      <Tag type="payment" />
+    </div>
+    <div class="cell">
+      <div class="left"><Tag type="disburment" /></div>
+      <div class="content">
+
+      </div>
+      <div class="right"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,3 +25,21 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.transactios {
+  height: rem-calc(104);
+  .cell {
+    display: flex;
+    .left {
+      flex: initial;
+    }
+    .content {
+      flex: 1;
+    }
+    .right {
+      flex: initial;
+    }
+  }
+}
+</style>
